1f91e379c0e4e0d82d52241ea73ce7f47acdce48
Matt Thomas <matt@betweenbrain.com>
2015-12-10 11:00:45 -0500

    Added basic controller

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,10 +1,12 @@
 /**
  * File
- * Created    12/10/15 10:36 AM
+ * Created    12/10/15 10:52 AM
  * Author     Matt Thomas | matt@betweenbrain.com | http://betweenbrain.com
  * Support    https://github.com/betweenbrain/
  * Copyright  Copyright (C) 2015 betweenbrain llc. All Rights Reserved.
  * License    GNU GPL v2 or later
  */
 
-var app = angular.module('helloApp', []);
+app.controller('MainController', ['$scope', function($scope) {
+  $scope.title = 'Hello Angular';
+}]);
