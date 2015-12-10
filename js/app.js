a492b14335515b9053e392a3e6b50986cf9dff5c
Matt Thomas <matt@betweenbrain.com>
2015-12-10 10:48:27 -0500

    Seeding basic Angular app

---
+++ js/app.js
@@ -0,0 +1,9 @@
+/**
+ * File
+ * Created    12/10/15 10:36 AM
+ * Author     Matt Thomas | matt@betweenbrain.com | http://betweenbrain.com
+ * Support    https://github.com/betweenbrain/
+ * Copyright  Copyright (C) 2015 betweenbrain llc. All Rights Reserved.
+ * License    GNU GPL v2 or later
+ */
+
